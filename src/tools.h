c2324f0269b7b7a8f3fc72dc437e1f29aea9aa94
Alexei Strots <strotz@gmail.com>
2017-05-12 12:47:40 -0700

    add simple delay measure tool

--- src/tools.h
+++ src/tools.h
@@ -1,6 +1,7 @@
 #ifndef TOOLS_H_
 #define TOOLS_H_
 #include <vector>
+#include <iostream>
 #include "Eigen/Dense"
 
 class Tools {
@@ -20,6 +21,27 @@ public:
   */
   Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, const std::vector<Eigen::VectorXd> &ground_truth);
 
+};
+
+class dl
+{
+public:
+
+  dl(const char* place) :
+    place_(place),
+    start_(clock())
+  {
+  }
+
+  ~dl() {
+    double elapsed_secs = double(clock() - start_) / CLOCKS_PER_SEC;
+    if (elapsed_secs > 1.0) {
+      std::cout << place_ << " " << elapsed_secs << std::endl;
+    }
+  }
+
+  const char* place_;
+  clock_t start_;
 };
 
 #endif /* TOOLS_H_ */
